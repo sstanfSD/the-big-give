c759f23fbab3c1becc82466d75f259154e18694b
Scott Stanfel <scottstanfel@Scotts-MacBook-Pro.local>
2022-11-22 12:32:02 +0900

    more edits

--- src/components/Home/Header/HeaderElements.js
+++ src/components/Home/Header/HeaderElements.js
@@ -68,6 +68,10 @@ export const HeadingBg = styled.div`
 
     object-fit: fill;
   }
+
+  & .gatsby-image-wrapper {
+    height: 100%;
+  }
 `
 export const Heading = styled.h1`
   margin: 2rem 1rem 0;
@@ -107,9 +111,10 @@ export const DateBg = styled.div`
 
   & img {
     width: 100%;
-    height: 100%;
-
     object-fit: fill;
+  }
+
+  & .gatsby-image-wrapper {
   }
 `
 
